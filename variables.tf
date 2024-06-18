3b9d9efce61b59d02a5964a2be3a39f125dac768
Galvao <ruan.jose.galvao@accenture.com>
2024-06-18 10:14:21 -0300

    teste esteira

--- variables.tf
+++ variables.tf
@@ -2,15 +2,13 @@ variable "description" {
   type = string
   default = "value"
 }
-variable "enviroment" {
-}
-
 variable "sg_example" {
   type = string
   default = "teste-sg"
 }
 variable "vpc_id" {
   type = string
+  default = "abc-123"
 }
 
 
